343ace2754fb034c3b2d29b27c213cb04e686aa5
herenpeng <2519832582@qq.com>
2021-07-31 19:45:04 +0800

    :lipstick:主页面标签显示逻辑

--- src/renderer/store/modules/main.js
+++ src/renderer/store/modules/main.js
@@ -1,78 +1,22 @@
 const state = {
-  showCommand: false,
-  databaseName: null,
-  tabNames: [],
-  currentTab: 'command',
-  tableData: {}
+  databaseName: null
 }
 
 const mutations = {
-  OPEN_COMMAND (state) {
-    state.showCommand = true
-    state.currentTab = 'command'
-  },
-  CLOSE_COMMAND (state) {
-    state.showCommand = false
-    if (state.tabNames.length > 0) {
-      state.currentTab = state.tabNames[0]
-    } else {
-      state.currentTab = 'command'
-    }
-  },
   SET_DATABASE_NAME (state, databaseName) {
     state.databaseName = databaseName
   },
   CLOSE_DATABASE (state) {
     state.databaseName = null
-  },
-  ADD_TAB_NAME (state, tabName) {
-    if (state.tabNames.indexOf(tabName) === -1) {
-      state.tabNames.push(tabName)
-    }
-    state.currentTab = tabName
-  },
-  DELETE_TAB_NAME (state, tabName) {
-    state.tabNames.forEach((tab, index) => {
-      if (tab === tabName) {
-        state.tabNames.splice(index, 1)
-        state.tableData[tabName] = null
-      }
-    })
-    if (state.currentTab === tabName) {
-      if (state.tabNames.length > 0) {
-        state.currentTab = state.tabNames[0]
-      } else {
-        state.currentTab = 'command'
-      }
-    }
-  },
-  SET_TABLE_DATA (state, {tabName, tableData}) {
-    state.tableData[tabName] = tableData
   }
 }
 
 const actions = {
-  openCommand ({commit}) {
-    commit('OPEN_COMMAND')
-  },
-  closeCommand ({commit}) {
-    commit('CLOSE_COMMAND')
-  },
   setDatabaseName ({commit}, databaseName) {
     commit('SET_DATABASE_NAME', databaseName)
   },
   closeDatabase ({commit}) {
     commit('CLOSE_DATABASE')
-  },
-  openTab ({commit}, tabName) {
-    commit('ADD_TAB_NAME', tabName)
-  },
-  closeTab ({commit}, tabName) {
-    commit('DELETE_TAB_NAME', tabName)
-  },
-
-  setTableData ({commit}, {tabName, tableData}) {
-    commit('SET_TABLE_DATA', {tabName, tableData})
   }
 }
 
